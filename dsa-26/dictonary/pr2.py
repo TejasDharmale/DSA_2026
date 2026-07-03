b4fa9797d9f5d62a58e742923343c7aa7d911e54
Tejas Dharmale <97274893+TejasDharmale@users.noreply.github.com>
2026-07-03 15:12:10 +0530

    Add dictionary exercise scripts pr2 and pr3

---
+++ dsa-26/dictonary/pr2.py
@@ -0,0 +1,5 @@
+dict1 = {'a': 10, 'b': 20, 'c': 30}
+dict2 = {'b': 5, 'c': 15, 'd': 40}
+
+dict1.update(dict2)
+print(dict1)
---
+++ dsa-26/dictonary/pr3.py
@@ -0,0 +1,15 @@
+data = [("Alice", 85), ("Bob", 92), ("Charlie", 78), ("Alice", 95)]
+result_dict = dict(data)
+print(result_dict)
+
+high_score = 0
+name = ""
+for i in result_dict:
+    result_dict[i]
+    if result_dict[i]> high_score:
+        high_score = result_dict[i]
+        name = i
+print("Highest scorer:", name)
+print("Score:", high_score)
+    
+    
