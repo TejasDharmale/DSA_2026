b964345d28114b41cc0361745ba6d2c5d4842b90
Tejas Dharmale <97274893+TejasDharmale@users.noreply.github.com>
2026-07-04 16:25:15 +0530

    Add pattern15.py and pattern16.py

---
+++ dsa-26/pattern16.py
@@ -0,0 +1,14 @@
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(j+1,end="")
+    for j in range(2*(n-i)-2):
+        print(" ",end="")
+    for j in range(i+1):
+        print(j+1,end="")
+    
+    
+    print()    
+        
+        
+    
