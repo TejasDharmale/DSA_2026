397bbaa0718665cbedb506be49a79a0e267cbeca
Tejas Dharmale <97274893+TejasDharmale@users.noreply.github.com>
2026-07-29 17:18:55 +0530

    Create last_&_first__occur_count.py

---
+++ dsa-26/binary search/last_&_first__occur_count.py
@@ -0,0 +1,39 @@
+class Solution:
+    def countOccurrences(self, nums, target):
+        start = 0
+        end = len(nums)-1
+        first = -1
+        last = -1
+
+        while(start<=end):
+            mid = start +(end-start)//2
+
+            if target == nums[mid]:
+                first = mid
+                end = mid-1
+            
+            elif nums[mid]>target:
+                end = mid-1
+            else:
+                start = mid+1
+                
+        start = 0
+        end = len(nums)-1        
+        while(start<=end):
+            mid = start +(end-start)//2
+
+            
+
+            if target == nums[mid]:
+                last = mid
+                start= mid+1
+            
+            elif nums[mid]>target:
+                end = mid-1
+            else:
+                start = mid+1        
+            
+        if first == -1:
+            return 0
+            
+        return last - first + 1      
