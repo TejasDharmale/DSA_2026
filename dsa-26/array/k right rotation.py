14dc56397882e74bf995b3652460bc20dd66bd88
Tejas Rajesh Dharmale <97274893+TejasDharmale@users.noreply.github.com>
2026-09-17 11:39:46 +0200

    Update k right rotation.py

--- dsa-26/array/k right rotation.py
+++ dsa-26/array/k right rotation.py
@@ -1,1 +1,12 @@
-print("hi")
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        n = len(nums)
+       
+        for j in range(0,k):
+            last= nums[-1]
+            for i in range(n-2,-1,-1):
+                nums[i+1]=nums[i]
+            nums[0]=last   
+        return nums        
+
+        
